48216d102b8d7280a33fc6c9d2ca59060da1c4a4
Jesus Medina <jesus.medina05@gmail.com>
2024-12-10 11:38:32 -0300

    add actioncable for comunication server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import RestaurantList from "./components/RestaurantList";
+import { cable } from "./actioncable";
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
     fetchRestaurants();
+
+    const subscription = cable.subscriptions.create("DeviceUpdatesChannel",{
+      received: (data) => {
+        console.log("Se recibe la actualizacion desde la api", data);
+        fetchRestaurants();
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    }
   }, []);
 
   const fetchRestaurants = async () => {
